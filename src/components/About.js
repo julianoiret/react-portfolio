0fc1e621f8ec23de824cb6c54a9882b5e30727ec
Julia Hanard <julia.hanard@rea-group.com>
2022-12-16 16:14:05 +1100

    typo mistake

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
                 My dedication to coding brought me to undertake a web development bootcamp - <a href="https://www.lewagon.com/" target="_blank" rel="noreferrer"><nobr>Le Wagon Melbourne</nobr></a>.<br />
                 Throughout this tech journey, I developed a strong interest in front-end development with a focus on the user interface and experience. 
                 I'm currently working at <a href="https://www.rea-group.com/" target="_blank" rel="noreferrer">REA Group</a>, ranked one of the best places to work in Australia and one of the best workplaces for women. I have been selected for the <a href="https://www.rea-group.com/careers/springboard-to-tech-program/" target="_blank" rel="noreferrer"><nobr>Springboard to Tech</nobr></a> program where I had the opportunity to learn and work within a large range of tech stacks such as Typescript, React, GraphQL, Kotlin, Scala, AWS & more, through 2 rotations, in 12 months.<br />
-                I'm passionnate, value my strong work ethic, promote Fair go and am environmentally conscious. In addition, I'm constantly looking for growth and learning opportunities.</p>
+                I'm passionate, value my strong work ethic, promote Fair go and am environmentally conscious. In addition, I'm constantly looking for growth and learning opportunities.</p>
             </div>
         </div>
         </>
